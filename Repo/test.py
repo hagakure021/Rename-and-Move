f4426e676a4425708b5d01bdab1d645e52adccdb
William Jones <william.c1.jones@gmail.com>
2023-04-05 20:12:20 -0400

    Update test.py
    able to name properly

--- Repo/test.py
+++ Repo/test.py
@@ -1,14 +1,15 @@
 import os
 from pathlib import Path
 import shutil
+import itertools
 
 # folder path
-dir_path = os.path.abspath (r'C:\Users\Admin\Documents\GitHub\Rename-and-Move\Repo\TEST')
+dir_path = os.path.abspath(r'C:\Users\Admin\Desktop\Repo\TEST')
 count = 0
 directory = "FOLDER_{}"
-test2_path: str = r'C:\Users\Admin\Documents\GitHub\Rename-and-Move\Repo\test2'
+test2_path: str = r':\Users\Admin\Desktop\Repo\test2'
 
- 
+
 def check_dir():
     count = 0
     # checking the directory for files
@@ -25,6 +26,7 @@ def check_dir():
         rname()
         folcreate()
 
+
 def folcreate():
     print("There are still files in this directory.")
     os.mkdir(dir_path + directory.format(count + 1))
@@ -32,31 +34,32 @@ def folcreate():
     move_file()
     # Loops back to begging until all files are removed from the dir_path
     check_dir()
-    rname()
+
+
 
 def move_file():
     file_names = os.listdir(dir_path)
     print("Moving files 12 at a time.")
     for file_names in file_names[:12]:
-       # need to figure out the proper way to move to just created folder
+        # need to figure out the proper way to move to just created folder
         shutil.move(os.path.join(dir_path, file_names), test2_path)
+
 
 # # need to rename the files extension to incrimneting number in 2 digit format
 def rname(f=None):
     os.chdir(dir_path)
     print("Renaming Files.")
     print(os.getcwd())
-    for count, f in enumerate(os.listdir()):
+    for index, f in enumerate(os.listdir()):
         f_name = os.path.splitext(f)[0]
-        f_name = (f + str(count + 1))
-       # f_name = (f + str(count + 1))
-        br = (f_name[0:7] + str((count + 1, "{:02d}".format(count))))
-        #print(f_name)
-        print(br)
-  #     new_name = f'{f_name}'
-   #     print("break here")
-    #    print(new_name)
-       # os.rename(f, new_name)
+        print(f_name)
+        ext_name = os.path.splitext(f)[1]
+        print(ext_name)
+        br = f_name[0:7] + "{:02d}".format((index % 12) + 1)
+        print(f_name)
+        new_name = f'{br}{ext_name}'
+        print(new_name)
+        os.rename(f, new_name)
 
-#main
+# main
 check_dir()
