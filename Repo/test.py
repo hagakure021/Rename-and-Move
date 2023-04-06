b2364863c07b39e8f8f83cdb21d3f01a1ee2317d
William Jones <william.c1.jones@gmail.com>
2023-04-05 21:46:32 -0400

    u

--- Repo/test.py
+++ Repo/test.py
@@ -1,13 +1,12 @@
 import os
 from pathlib import Path
 import shutil
-import itertools
 
 # folder path
-dir_path = os.path.abspath(r'C:\Users\Admin\Desktop\Repo\TEST')
+dir_path = os.path.abspath(r'C:\Users\Admin\Documents\GitHub\Rename-and-Move\Repo\TEST')
 count = 0
 directory = "FOLDER_{}"
-test2_path: str = r':\Users\Admin\Desktop\Repo\test2'
+test2_path: str = r'C:\Users\Admin\Documents\GitHub\Rename-and-Move\Repo\test2'
 
 
 def check_dir():
@@ -24,42 +23,46 @@ def check_dir():
         print("There are no files in the directory.")
     else:
         rname()
-        folcreate()
 
+
+
+def rname():
+    os.chdir(dir_path)
+    print("Renaming Files.")
+    #print(os.getcwd())
+    for index, f in enumerate(os.listdir()):
+        f_name = os.path.splitext(f)[0]
+        ext_name = os.path.splitext(f)[1]
+        br = f_name[0:7] + "{:02d}".format((index % 12) + 1)
+        new_name = f'{br}{ext_name}'
+        os.rename(f, new_name)
+    print("Moving to folder create")
+    folcreate()
+   # move_file()
 
 def folcreate():
     print("There are still files in this directory.")
-    os.mkdir(dir_path + directory.format(count + 1))
+    isExist = os.path.exists(dir_path + directory.format(count + 1))
+    print(isExist)
+    os.mkdir(dir_path + directory.format(isExist + 1))
     print("Directory Created")
+    print("Moving to move file")
     move_file()
+
     # Loops back to begging until all files are removed from the dir_path
-    check_dir()
-
-
 
 def move_file():
     file_names = os.listdir(dir_path)
+    print(file_names)
     print("Moving files 12 at a time.")
     for file_names in file_names[:12]:
         # need to figure out the proper way to move to just created folder
         shutil.move(os.path.join(dir_path, file_names), test2_path)
+    print('Finishing move restarting check')
+    check_dir()
 
 
-# # need to rename the files extension to incrimneting number in 2 digit format
-def rname(f=None):
-    os.chdir(dir_path)
-    print("Renaming Files.")
-    print(os.getcwd())
-    for index, f in enumerate(os.listdir()):
-        f_name = os.path.splitext(f)[0]
-        print(f_name)
-        ext_name = os.path.splitext(f)[1]
-        print(ext_name)
-        br = f_name[0:7] + "{:02d}".format((index % 12) + 1)
-        print(f_name)
-        new_name = f'{br}{ext_name}'
-        print(new_name)
-        os.rename(f, new_name)
+
 
 # main
 check_dir()
